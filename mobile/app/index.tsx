594df7af976ea3b89739629cd243d693d8e9f32c
harinaikm <harinaik.m@dhatvibs.com>
2025-11-11 12:08:52 +0530

    add tailwind

---
+++ mobile/app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="">
+      <Text className="text-red-600 ">
+        Edit app/index.tsx to edit this screen.
+      </Text>
+    </View>
+  );
+}
